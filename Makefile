841dbd373c9a8afff9acf63c5122c16ddbe2ab9b
Kræn Hansen <post@creen.dk>
2013-07-11 18:38:23 +0200

    A days work ..

--- Makefile
+++ Makefile
@@ -1,20 +1,27 @@
-build: bootstrap-git-update overwrite-variables update-bootstrap
+build: update-bootstrap update-jquery.snapscroll
 	@echo "Building the Bit Blueprint Frontpage"
 	recess --compile ./less/main.less > ./css/main.css
 	recess --compress ./less/main.less > ./css/main.min.css
+	@echo "All done ... "
 
-update-bootstrap:
+update-bootstrap: bootstrap-git-update overwrite-variables
 	cd bootstrap && make clean; make bootstrap
 	cp -f bootstrap/bootstrap/css/*.css ./css
 	cp -f bootstrap/bootstrap/js/*.js ./js
 	cp -f bootstrap/bootstrap/img/* ./img
-	@echo "All done ... "
+
+update-jquery.snapscroll: jquery.snapscroll-git-update
+	cp -f jquery.snapscroll/core/jquery.snapscroll.min.js ./js
+	cp -f jquery.snapscroll/core/dependencies/jquery.scroll_to.js ./js
 
 overwrite-variables:
 	cp -f less/variables.less bootstrap/less/
 
 bootstrap-git-update:
 	git submodule update --init bootstrap
+
+jquery.snapscroll-git-update:
+	git submodule update --init jquery.snapscroll
 
 watch:
 	echo "Watching less files..."; \
--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 (function($) {
+	$('body').snapscroll();
 	function adjustPageHeights(e) {
 		// Make sure that a page is always as high as the window.
 		var windowHeight = $(window).height();
@@ -6,4 +7,38 @@
 	}
 	$(window).resize(adjustPageHeights);
 	adjustPageHeights();
+
+	$("a[href^='#']").click(function(e) {
+		e.preventDefault();
+		var hash = $(e.target).attr("href");
+		if(!hash) hash = "";
+		hash = hash.substring(1);
+		$.scrollTo("a[name='"+ hash +"']", 300);
+	});
+	
+	if(history.pushState) { // check if the browser supports the pushState
+		$(window).scroll(function(e) {
+			$this = $(this);
+			if($this.data('scrollUpdateHashTimeout')) {
+				clearTimeout($this.data('scrollUpdateHashTimeout'));
+			}
+			$this.data('scrollUpdateHashTimeout', setTimeout(function() {
+				var scrollTop = $(window).scrollTop();
+				var closestAnchor = null;
+				var closestAnchorOffsetTop = null;
+				// Loop though anchors to see which is closest.
+				$("a[name]").each(function() {
+					var offsetTop = $(this).offset().top;
+					if(closestAnchor == null || Math.abs(scrollTop - closestAnchorOffsetTop) > Math.abs(scrollTop - offsetTop)) {
+						closestAnchor = this;
+						closestAnchorOffsetTop = offsetTop;
+					}
+				});
+				if(closestAnchor) {
+					// Update the hash.
+					history.pushState({}, "", "#"+$(closestAnchor).attr("name"));
+				}
+			}, 100));
+		});
+	}
 })(jQuery);
