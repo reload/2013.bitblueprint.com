426c21cce5b3c2e4c6215d7e49b57e7ea1173626
Kræn Hansen (for DR A&R Innovation) <kh@bitblueprint.com>
2013-08-28 14:11:24 +0200

    Fixing resize errors.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 (function($) {
 	function adjustPageHeights(e) {
+		var hash = location.hash.substring(1);
 		// Make sure that a page is always as high as the window.
 		var windowHeight = $(window).height();
 		$(".page").css("min-height", windowHeight);
+		var $anchor = $("a[name='"+ hash +"']");
+		$(window).data('currentAnchor', $anchor);
+		// Scroll the anchor in previous focus.
+		$.scrollTo($anchor, 0, {offset: {top: -1}});
+		updateNavbar();
 	}
 	$("a[href^='#']").click(function(e) {
 		e.preventDefault();
@@ -15,27 +21,32 @@
 	function updatePage() {
 		var scrollTop = $(window).scrollTop();
 		var navbarHeight = $("body > .navbar").height();
-		var currentAnchor = null;
+		var $currentAnchor = null;
 		// Loop though anchors to see which is the last above the viewport.
 		$("a[name]").each(function() {
 			var offsetTop = $(this).offset().top;
 			if(scrollTop - offsetTop + navbarHeight >= 0) {
-				currentAnchor = this;
+				$currentAnchor = $(this);
 			}
 		});
-		if(currentAnchor && (!$(window).data('currentAnchor') || $(window).data('currentAnchor') != currentAnchor)) {
-			$(window).data('currentAnchor', currentAnchor);
+		if($currentAnchor && (!$(window).data('currentAnchor') || !$(window).data('currentAnchor').is($currentAnchor))) {
+			$(window).data('currentAnchor', $currentAnchor);
 			if(history.pushState) { // check if the browser supports the pushState
 				// Update the hash.
-				history.pushState({}, "", "#"+$(currentAnchor).attr("name"));
+				history.pushState({}, "", "#"+$currentAnchor.attr("name"));
 			}
-			// Update the navbar classes.
-			var navbarColor = $(currentAnchor).data("navbar-color");
-			$("body > .navbar").removeClass("white blue marine black").addClass(navbarColor);
+			updateNavbar();
 		}
 	}
+
+	function updateNavbar() {
+		var $currentAnchor = $(window).data('currentAnchor');
+		// Update the navbar classes.
+		var navbarColor = $currentAnchor.data("navbar-color");
+		$("body > .navbar").removeClass("white blue marine black").addClass(navbarColor);
+	}
 	
+	adjustPageHeights();
 	$(window).scroll(updatePage);
 	$(window).resize(adjustPageHeights);
-	adjustPageHeights();
 })(jQuery);
