437043c7ad6fe81d23ec785551b031adcb1bf5b0
Kræn Hansen (for DR A&R Innovation) <kh@bitblueprint.com>
2013-09-08 18:46:58 +0200

    Removed bootstraps. .

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-build: update-bootstrap update-jquery.snapscroll
+build: update-bootstrap
 	@echo "Building the Bit Blueprint Frontpage"
 	recess --compile ./less/main.less > ./css/main.css
 	recess --compress ./less/main.less > ./css/main.min.css
@@ -10,22 +10,12 @@ update-bootstrap: bootstrap-git-update overwrite-variables
 	cp -f bootstrap/bootstrap/js/*.js ./js
 	cp -f bootstrap/bootstrap/img/* ./img
 
-update-jquery.snapscroll: jquery.snapscroll-git-update
-	cp -f jquery.snapscroll/core/jquery.snapscroll.min.js ./js
-	cp -f jquery.snapscroll/core/dependencies/jquery.scroll_to.js ./js
-
 overwrite-variables:
 	cp -f less/variables.less bootstrap/less/
 
 bootstrap-git-update:
 	git submodule update --init bootstrap
 
-jquery.snapscroll-git-update:
-	git submodule update --init jquery.snapscroll
-
 watch:
 	echo "Watching less files..."; \
 	watchr -e "watch('less/.*\.less') { system 'make' }"
-
-pull-and-upload:
-	../pull-and-upload.sh
--- js/main.js
+++ js/main.js
@@ -1,26 +1,27 @@
 (function($) {
-	function adjustPageHeights(e) {
-		var hash = location.hash.substring(1);
-		// Make sure that a page is always as high as the window.
-		var windowHeight = $(window).height();
-		$(".page").css("min-height", windowHeight);
-		var $anchor = $("a[name='"+ hash +"']");
-		$(window).data('currentAnchor', $anchor);
-		// Scroll the anchor in previous focus.
-		$.scrollTo($anchor, 0, {offset: {top: -1}});
-		updateNavbar();
-	}
+	// Old code.
+	// var hash = location.hash.substring(1);
+	// var $anchor = $("a[name='"+ hash +"']");
+	// $(window).data('currentAnchor', $anchor);
+	// Scroll the anchor in previous focus.
+	// $.scrollTo($anchor, 0, {offset: {top: -1}});
+	// updateNavbar();
+
+	// When some link with a href starting with the # is clicked.
 	$("a[href^='#']").click(function(e) {
 		e.preventDefault();
 		var hash = $(e.target).attr("href");
 		if(!hash) hash = "";
 		hash = hash.substring(1);
-		$.scrollTo("a[name='"+ hash +"']", 300, {offset: {top: -1}});
+		$.scrollTo("a[name='"+ hash +"']", 300, {offset: {top: 0}});
 	});
 
-	function updatePage() {
+	function updateCurrentAnchor() {
+		console.log("updateCurrentAnchor called ...");
 		var scrollTop = $(window).scrollTop();
 		var navbarHeight = $("body > .navbar").height();
+		// Remember the previous anchor for comparison.
+		var $previousAnchor = $(window).data('currentAnchor');
 		var $currentAnchor = null;
 		// Loop though anchors to see which is the last above the viewport.
 		$("a[name]").each(function() {
@@ -29,8 +30,10 @@
 				$currentAnchor = $(this);
 			}
 		});
-		if($currentAnchor && (!$(window).data('currentAnchor') || !$(window).data('currentAnchor').is($currentAnchor))) {
-			$(window).data('currentAnchor', $currentAnchor);
+		console.log("$currentAnchor = ", $currentAnchor.get(0));
+		// Update the anchor.
+		$(window).data('currentAnchor', $currentAnchor);
+		if($currentAnchor && ($previousAnchor == null || !$previousAnchor.is($currentAnchor))) {
 			if(history.pushState) { // check if the browser supports the pushState
 				// Update the hash.
 				history.pushState({}, "", "#"+$currentAnchor.attr("name"));
@@ -39,14 +42,30 @@
 		}
 	}
 
-	function updateNavbar() {
+	function adjustPageHeights() {
+		var $previousAnchor = $(window).data('currentAnchor');
+		// Make sure that a page is always as high as the window.
+		var windowHeight = $(window).height();
+		$(".page").css("min-height", windowHeight);
+		updateCurrentAnchor();
 		var $currentAnchor = $(window).data('currentAnchor');
-		// Update the navbar classes.
+		// If the current anchor has changed due to an ajustment in page height ..
+		if($previousAnchor && !$previousAnchor.is($currentAnchor)) {
+			// Scroll to the previousAnchor.
+			$.scrollTo($previousAnchor, 0, {offset: {top: 0}});
+		}
+	}
+
+	// Update the class on the navbar, based on the current anchor.
+	function updateNavbar() {
+		// Get the current anchor from the window's data.
+		var $currentAnchor = $(window).data('currentAnchor');
+		// What is the color for the navbar?
 		var navbarColor = $currentAnchor.data("navbar-color");
+		// Update the navbar classes.
 		$("body > .navbar").removeClass("white blue marine black").addClass(navbarColor);
 	}
 	
-	adjustPageHeights();
-	$(window).scroll(updatePage);
-	$(window).resize(adjustPageHeights);
+	$(window).scroll(updateCurrentAnchor);
+	$(window).resize(adjustPageHeights).resize();
 })(jQuery);
