9661c97bf1c8a9a822f488a1afdb45a69384ca85
Kræn Hansen (for DR A&R Innovation) <kh@bitblueprint.com>
2013-09-09 14:41:18 +0200

    Deactivating updateCurrentAnchor while scrollTo from menu is getting processed.

--- js/main.js
+++ js/main.js
@@ -13,30 +13,41 @@
 		var hash = $(e.target).attr("href");
 		if(!hash) hash = "";
 		hash = hash.substring(1);
-		$.scrollTo("a[name='"+ hash +"']", 500, {offset: {top: 0}});
+
+		$(window).data('scrollToActive', true);
+		$.scrollTo("a[name='"+ hash +"']", 500, {
+			offset: {top: 0},
+			onAfter: function() {
+				$(window).data('scrollToActive', false);
+				updateCurrentAnchor();
+			}
+		});
 	});
 
 	function updateCurrentAnchor() {
-		var scrollTop = $(window).scrollTop();
-		var navbarHeight = $("body > .navbar").height();
-		// Remember the previous anchor for comparison.
-		var $previousAnchor = $(window).data('currentAnchor');
-		var $currentAnchor = null;
-		// Loop though anchors to see which is the last above the viewport.
-		$("a[name]").each(function() {
-			var offsetTop = $(this).offset().top;
-			if(scrollTop - offsetTop + navbarHeight >= 0) {
-				$currentAnchor = $(this);
+		var scrollToActive = $(window).data('scrollToActive') == true;
+		if(!scrollToActive) {
+			var scrollTop = $(window).scrollTop();
+			var navbarHeight = $("body > .navbar").height();
+			// Remember the previous anchor for comparison.
+			var $previousAnchor = $(window).data('currentAnchor');
+			var $currentAnchor = null;
+			// Loop though anchors to see which is the last above the viewport.
+			$("a[name]").each(function() {
+				var offsetTop = $(this).offset().top;
+				if(scrollTop - offsetTop + navbarHeight >= 0) {
+					$currentAnchor = $(this);
+				}
+			});
+			// Update the anchor.
+			$(window).data('currentAnchor', $currentAnchor);
+			if($previousAnchor && $currentAnchor && ($previousAnchor == null || !$previousAnchor.is($currentAnchor))) {
+				if(history.pushState) { // check if the browser supports the pushState
+					// Update the hash.
+					history.pushState({}, "", "#"+$currentAnchor.attr("name"));
+				}
+				updateNavbar();
 			}
-		});
-		// Update the anchor.
-		$(window).data('currentAnchor', $currentAnchor);
-		if($previousAnchor && $currentAnchor && ($previousAnchor == null || !$previousAnchor.is($currentAnchor))) {
-			if(history.pushState) { // check if the browser supports the pushState
-				// Update the hash.
-				history.pushState({}, "", "#"+$currentAnchor.attr("name"));
-			}
-			updateNavbar();
 		}
 	}
 
@@ -48,9 +59,16 @@
 		updateCurrentAnchor();
 		var $currentAnchor = $(window).data('currentAnchor');
 		// If the current anchor has changed due to an ajustment in page height ..
+		$(window).data('scrollToActive', true);
 		if($previousAnchor && !$previousAnchor.is($currentAnchor)) {
 			// Scroll to the previousAnchor.
-			$.scrollTo($previousAnchor, 0, {offset: {top: 0}});
+			$.scrollTo($previousAnchor, 0, {
+				offset: {top: 0},
+				onAfter: function() {
+					$(window).data('scrollToActive', false);
+					updateCurrentAnchor();
+				}
+			});
 		}
 	}
 
@@ -64,6 +82,7 @@
 		$("body > .navbar").removeClass("white blue marine black").addClass(navbarColor);
 	}
 	
+	$(window).data('scrollToActive', false);
 	$(window).scroll(updateCurrentAnchor);
 	$(window).resize(adjustPageHeights).resize();
 })(jQuery);
